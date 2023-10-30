f24cb0a64558642424700352b4b77689c13546c2
Jake Martin <jakemart@usc.edu>
2023-10-29 21:02:57 -0700

    started creating profile page and shared background for all pages

---
+++ app/src/main/java/com/example/talk2friends/ProfileActivity.java
@@ -0,0 +1,19 @@
+package com.example.talk2friends;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+
+
+
+
+}
